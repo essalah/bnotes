6ca5482548aa3ebabb0e13a5c51a987219e4a9bf
Tair Rzayev <tair.rzayev@gmail.com>
2016-08-29 18:01:52 +0300

    Move framework-indemendent test under tests

--- app/src/test/java/com/tr/expenses/CurrencyUtilTest.java
+++ app/src/test/java/com/tr/expenses/CurrencyUtilTest.java
@@ -2,9 +2,11 @@ package com.tr.expenses;
 
 import com.tr.bnotes.util.CurrencyUtil;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
 
-public class CurrencyUtilTest extends TestCase {
+public class CurrencyUtilTest {
+    @Test
     public void testCurrencyConverter() {
         assertEquals(9999, CurrencyUtil.fromString("$@#!$@99.99$#%"));
         assertEquals("99.99", CurrencyUtil.toUnsignedCurrencyString(9999));
